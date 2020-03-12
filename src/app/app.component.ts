1dde8d64bdccbcfebfa6eaf5f24000da7a4e0415
Tidi <rtidiane@gmail.com>
2020-03-12 18:23:18 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-project-issue12540';
+
+  happyControl = new FormControl(null);
+  
+  sadControl = new FormControl(null);
+
+  form = new FormGroup({
+    happy: this.happyControl,
+    sad: this.sadControl,
+  });
+
+  ngOnInit(): void {
+    this.happyControl.valueChanges
+    .subscribe(x => console.log("Happy (" + typeof x + "): " + x));
+
+    this.sadControl.valueChanges
+    .subscribe(x => console.log("Sad (" + typeof x + "): " + x));
+
+  }
 }
